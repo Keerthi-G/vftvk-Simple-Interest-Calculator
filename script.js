94c9016db13c9493308653159fd58b025652a1ae
Keerthi-G <keerthiganesan712@gmail.com>
2021-06-11 18:26:14 +0000

    changes made for final project submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if(principal<=0){
     alert("Enter a positive number")
 }
 else{
-document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>"+",\<br\>at an interest rate of "+"\<mark>"+rate+"\</mark>"+"%.\<br\>You will receive an amount of "+"\<mark>"+amount+",\<br\>in the year "+year+"\</mark>"+".\<br\>" 
+document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>"+",\<br\>at an interest rate of "+"\<mark>"+rate+"\</mark>"+"%.\<br\>You will receive an amount of "+"\<mark>"+amount+"\</mark>"+",\<br\>in the year "+"\<mark>"+year+"\</mark>"+".\<br\>" 
 }
 }
 function updateRate() 
