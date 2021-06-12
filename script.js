83621519a59585127d2a04b05638926b6e92f135
Keerthi-G <keerthiganesan712@gmail.com>
2021-06-12 10:21:25 +0000

    changes made for final project submission

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ var year = new Date().getFullYear()+parseInt(years);
 var amount = principal * years * rate /100; 
 if(principal<=0){
     alert("Enter a positive number")
+    document.getElementById("principal").focus();
 }
 else{
-document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>"+",\<br\>at an interest rate of "+"\<mark>"+rate+"\</mark>"+"%.\<br\>You will receive an amount of "+"\<mark>"+amount+"\</mark>"+",\<br\>in the year "+"\<mark>"+year+"\</mark>"+".\<br\>" 
+document.getElementById("result").innerHTML="If you deposit \<mark>"+principal+"\</mark>"+",\<br\>at an interest rate of "+"\<mark>"+rate+"%\</mark>.\<br\>You will receive an amount of "+"\<mark>"+amount+"\</mark>"+",\<br\>in the year "+"\<mark>"+year+"\</mark>"+".\<br\>\<br\>" 
 }
 }
 function updateRate() 
